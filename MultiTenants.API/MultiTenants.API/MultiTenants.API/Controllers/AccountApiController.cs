fcb97fd0c9527c088ee627ccb7ddd2186cafcfb0
Sumbal Ayaz <sumbal.ayaz764@gmail.com>
2025-12-22 18:08:37 +0500

    initial - created web and API project
    - Created Web Asp.Net core project
    - Creayed asp.net core API project

---
+++ MultiTenants.API/MultiTenants.API/MultiTenants.API/Controllers/AccountApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiTenants.API.Models;
+
+namespace MultiTenants.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountApiController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody]LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (model.UserName == "Admin" && model.Email == "admin@test.com" || model.UserName == "User" && model.Email == "user@test.com")
+            {
+                return Ok(new { Success = true, Message = "Login successful" });
+
+            }
+            return Ok(new { Success = false, Message = "Invalid Username or email" });
+        }
+        [HttpPost("SignUp")]
+        public IActionResult SignUp([FromBody]SignUpViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+           
+            return Ok(new { Success = true, Message = "SignUp successful" });
+        }
+    }
+}
