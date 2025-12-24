c3d6c124d250ee372978f74d5e2a369ada9b0624
Muhammad Asim Shah <shahasim190@gmail.com>
2025-12-24 18:03:37 +0500

    Add NSwag and Redoc

--- MultiTenants.API/Program.cs
+++ MultiTenants.API/Program.cs
@@ -3,16 +3,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// implement NSwag for API documentation
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
 }
+
+app.UseOpenApi();      // /swagger/v1/swagger.json
+app.UseSwaggerUi();    // /swagger
+
+// Redoc HTML endpoint at /redoc
+app.MapGet("/redoc", async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.WriteAsync(@"
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>API Docs - Redoc</title>
+  </head>
+  <body>
+    <redoc spec-url='/swagger/v1/swagger.json'></redoc>
+
+    <!-- Load Redoc from CDN -->
+    <script src=""https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js""></script>
+  </body>
+</html>");
+});
 
 app.UseHttpsRedirection();
 
